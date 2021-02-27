c14e150f293fa7b2ccb3a93ae4654ecf57a220e3
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-28 00:59:17 +0600

    all commplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+// import fakeData from './data/data.json'
 
 
 function App() {
+
 
   return (
     <div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
+import fakeData from '../../data/data.json';
 
 const Header = () => {
+    const [fake, fakeAllData] = useState([]);
+    useEffect(() =>{
+      fakeAllData(fakeData);
+      console.log(fakeData);
+
+  
+    }, [])
+
     return (
         <div className="header">
             <img src={logo} alt=""/>
             <nav>
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
-                <a href="/manage">Manage Inventory</a>
+                <a href="/inventory">Orders</a>
             </nav>
         </div>
     );
